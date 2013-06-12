f8b76264ddc4a3af22fd226f470a09fac9ca1d17
Gustavo Gomes <github@guvux.com.br>
2013-06-12 15:54:36 -0500

    Publishing

---
+++ client/client.cpp
@@ -0,0 +1,91 @@
+/*========================================================================================================================================
+@GUSTAVO TEIXEIRA GOMES
+
+*Client TCP
+
+* To-do:
+* - improve error checking
+========================================================================================================================================*/
+
+#include "..\socket\socket.h"
+#include <fstream>
+
+#define _DEBUG_ 0
+
+using namespace std;
+
+int main() {
+
+  try {
+	// Define variables
+	ifstream inputFile;
+	ofstream outputFile;
+	char *inputBuffer;
+	string outputBuffer;
+	char *inputFileName = "../grip-client/worldData.txt";
+	char *outputFileName = "../grip-client/command.txt";
+		
+	// Connect to the sever
+    SocketClient server(SERVER_IP, SERVER_PORT);
+	
+	// Open and Send file
+	inputFile.open(inputFileName, std::ifstream::binary);
+	if (inputFile.is_open()) {
+		// Transfering the file worldData
+		cout << "Transfering the file" << endl;
+		while(inputFile.good()) {
+			inputBuffer = new char [MAXBUFFER];
+			inputFile.read(inputBuffer, MAXBUFFER);
+			if (_DEBUG_) {
+				cout << "Count: " << inputFile.gcount() << endl;
+				cout << inputBuffer << endl;
+			}
+			server.SendBytes(inputBuffer, (int)inputFile.gcount());
+			delete[] inputBuffer;
+		}
+		inputFile.close();
+		cout << "Ending Transfering the file" << endl;
+		
+		cout << "Receiving the file" << endl;
+		// Receiving the file command
+		outputFile.open(outputFileName, std::ofstream::binary);
+		bool stillConnected = true;
+		while (1) {
+			outputBuffer = server.ReceiveBytes();
+			if (_DEBUG_) cout << "Buffer: " << outputBuffer.size() << endl;
+			if (outputBuffer.empty()) {
+				break;
+			}
+			outputFile << outputBuffer;
+			outputFile.flush();
+			if (outputBuffer.size() < MAXBUFFER) {
+				break;
+			}
+		}
+		outputFile.flush();
+		outputFile.close();
+		cout << "Ending Receiving the file" << endl;
+	}
+	else {
+		cout << "Openning " << inputFileName << " file error" << endl;
+	}
+  }
+  catch (const char* s) {
+    cerr << s << endl;
+	if (_DEBUG_) system("pause");
+  } 
+  catch (std::string s) {
+    cerr << s << endl;
+	if (_DEBUG_) system("pause");
+  } 
+  catch (...) {
+    cerr << "unhandled exception\n";
+	if (_DEBUG_) system("pause");
+  }
+  if (_DEBUG_) {
+	  system("pause");
+	  system("comp ..\\grip-client\\worldData.txt ..\\icarus-server\\worldData.txt /A");
+	  system("comp ..\\grip-client\\command.txt ..\\icarus-server\\command.txt /A");
+  }
+  return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,77 @@
+/*========================================================================================================================================
+@GUSTAVO TEIXEIRA GOMES
+
+*Server TCP
+========================================================================================================================================*/
+
+#include "..\socket\socket.h"
+#include <fstream>
+#include <process.h>
+
+#define _DEBUG_ 0
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	// Define variables
+	ifstream inputFile;
+	ofstream outputFile;
+	char *inputBuffer;
+	string outputBuffer;
+	char *outputFileName = "../icarus-server/worldData.txt";
+	char *inputFileName = "../icarus-server/command.txt";
+			
+  	// Create socket
+  	SocketServer in(SERVER_PORT,5);
+
+	while (1) {
+	cout << "Waiting on port " << SERVER_PORT << endl;
+	
+	// Wait for the client
+    Socket* client = in.Accept();
+	
+	// Receiving the file worldData
+	cout << "Receiving the file" << endl;
+	outputFile.open(outputFileName, std::ofstream::binary);
+	while (1) {
+		outputBuffer = client->ReceiveBytes();
+		if (_DEBUG_) cout << "Buffer: " << outputBuffer.size() << endl;
+		if (outputBuffer.empty()) {
+			break;
+		}
+		outputFile << outputBuffer;
+		if (outputBuffer.size() < MAXBUFFER) {
+			break;
+		}
+	}
+	outputFile.flush();
+	outputFile.close();
+	cout << "Ending Receiving the file" << endl;
+		
+	cout << "Transfering the file" << endl;
+	// Open and Send file command
+	inputFile.open(inputFileName, std::ifstream::binary);
+	if (inputFile.is_open()) {
+		while(inputFile.good()) {
+			inputBuffer = new char [MAXBUFFER];
+			inputFile.read(inputBuffer, MAXBUFFER);
+			if (_DEBUG_) {
+				cout << "Count: " << inputFile.gcount() << endl;
+				cout << inputBuffer << endl;
+			}
+			client->SendBytes(inputBuffer, (int)inputFile.gcount());
+			delete[] inputBuffer;
+		}
+		inputFile.close();
+		cout.flush();
+		cout << "Ending Transfering the file" << endl;
+	}
+	else {
+		cout << "Openning " << inputFileName << " file error" << endl;
+	}
+	delete[] client;
+  }
+ 
+  return 0;
+}
